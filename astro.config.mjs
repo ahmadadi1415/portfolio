cdb5235550dac25f9d3cae31df02e3371c27f6ef
ahmadadi1415 <adipras606@gmail.com>
2023-11-14 23:31:33 +0700

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,11 @@ import { SITE_URL } from "./src/data/config";
 
 
 // https://astro.build/config
+//
 export default defineConfig({
   integrations: [tailwind(), sitemap(), robotsTxt()],
   site: SITE_URL,
-  base: '/',
+  base: '/', 
   markdown: {
     syntaxHighlight: "shiki",
     shikiConfig: {
